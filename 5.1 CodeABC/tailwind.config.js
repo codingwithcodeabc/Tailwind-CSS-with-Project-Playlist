73b4064cfbb7dff5ed91ed7e3fb7e333cc8582b3
Shahjalalrafi <shahjalalrafi5@gmail.com>
2023-09-16 11:10:50 +0600

    Biggest project uploaded

---
+++ 5.1 CodeABC/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#0092E4",
+        "secondary": "#183656"
+      }
+    },
+  },
+  plugins: [],
+}
